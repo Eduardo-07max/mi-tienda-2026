d9fe191ef08c33d6ce66b2cf298e32471927badb
Eduardo <quevedoedu2@gmail.com>
2026-04-23 13:30:23 -0600

    pre-deploy: preparacion de backend para railway

--- backend/backend-laravel/app/Providers/AppServiceProvider.php
+++ backend/backend-laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Esto fuerza a que todos los links internos usen HTTPS en producción
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
